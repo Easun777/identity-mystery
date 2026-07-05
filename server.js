e34f8f4106942cb4e157456342f7c31391ca3a61
Easun777 <chenyiyang19@mails.ucas.ac.cn>
2026-07-06 01:53:39 +0800

    Socket.io路径改为/api/io绕过Render拦截

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { path: '/api/io' });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
